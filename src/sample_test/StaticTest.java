b9cc99e89293baaeae583bc4306b312aa233e76d
Gavisiddappa Kampli <gavisiddappa.kampli@bytedance.com>
2022-08-21 17:40:48 +0800

    static and non static

---
+++ src/sample_test/StaticTest.java
@@ -0,0 +1,37 @@
+package sample_test;
+
+public class StaticTest {
+
+	// Class name always starts with upper case letter
+	// If there are more than one word in the class name
+	// each word should start with upper case letter
+	public static int a = 5; // a is a static variable
+	public int b = 10; // b is a non static variable
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// obj1 creation
+		StaticTest obj1 = new StaticTest();
+		System.out.println(obj1.a);
+		System.out.println(obj1.b);
+
+		// obj2 creation
+		StaticTest obj2 = new StaticTest();
+		System.out.println(obj2.a);
+		System.out.println(obj2.b);
+
+		obj1.a = 20; // static variable value change
+		System.out.println(obj1.a);
+		
+		obj1.b = 30;
+		System.out.println(obj2.b);
+
+	}
+
+}
+
+// area of circle
+// pi = 3.142
+// radius
+// Class Circek
---
+++ src/sample_test/TernaryOperatorTest.java
@@ -0,0 +1,31 @@
+package sample_test;
+
+public class TernaryOperatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// greater of 2 numbers
+		
+		int a = 6;
+		int b = 7;
+		
+		String result;
+		/*
+		if(a>b) {
+			System.out.println("a is greater than b");
+		}
+		else {
+			System.out.println("b is greater than a");
+		}
+		*/
+		// ternary operator
+		// (condition)?result1:result2;
+	 result =(a>b)?"a is greater":"b is greater";
+	
+		
+		
+
+	}
+
+}
